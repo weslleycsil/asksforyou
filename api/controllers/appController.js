6f6701eaa8f9b340a01a73eff6d7c295a9eeb6de
Weslley Silva <weslleycsil@gmail.com>
2022-01-06 20:28:05 -0300

    remoção de funcoes nao usadas

--- api/controllers/appController.js
+++ api/controllers/appController.js
@@ -10,27 +10,12 @@ exports.listAsks = (req,res) => {
     
 };
 
-exports.createAsk = (req,res) => {
-    res.render('pages/addAsks');
-};
-
-exports.index = (req,res) => {
-    res.render('pages/home');
-};
 
 exports.editAsk = (req,res) => {
     Asks.getByUUID(req.params.uuid)
     .then(ask =>{
         console.log(ask)
         res.render('pages/editAsk', {ask: ask});
-    })
-    .catch( err => logger.error(err))
-};
-
-exports.getAsk = (req, res) => {
-    Asks.getByReview(true)
-    .then(asks => {
-        res.render('pages/getAsk', {ask: asks[((Math.random() * asks.length) | 0)]})
     })
     .catch( err => logger.error(err))
 };
