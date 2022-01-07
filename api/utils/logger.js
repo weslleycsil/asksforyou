08e0f6cb8696b04dd81398d8e41d8c5fe4ac679d
Weslley Silva <weslleycsil@gmail.com>
2022-01-07 17:52:25 -0300

    Update logger.js

--- api/utils/logger.js
+++ api/utils/logger.js
@@ -6,6 +6,7 @@ if (config.env == 'test' || config.env == 'local' || config.env == 'development'
 	logger = console;
 } else {
 	//caso o ambiente seja produção
+	logger = console;
 }
 
 module.exports = logger;
